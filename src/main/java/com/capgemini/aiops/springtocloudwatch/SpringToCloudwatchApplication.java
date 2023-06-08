de449d4b015b90999a13e10bb82ca88800fa73c2
Robert Barrimond <robert@barrimond.com>
2023-06-08 18:09:19 +0000

    init

---
+++ src/main/java/com/capgemini/aiops/springtocloudwatch/SpringToCloudwatchApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.aiops.springtocloudwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToCloudwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringToCloudwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/aiops/springtocloudwatch/SpringToCloudwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.aiops.springtocloudwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringToCloudwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
