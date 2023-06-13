18b0cd3ca4773c3acd28bd604cd30721cd28581e
Robert Barrimond <robert@barrimond.com>
2023-06-13 14:45:08 +0000

    AWS Success!

--- src/main/java/com/capgemini/aiops/springtocloudwatch/SpringToCloudwatchApplication.java
+++ src/main/java/com/capgemini/aiops/springtocloudwatch/SpringToCloudwatchApplication.java
@@ -7,10 +7,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@EnableScheduling
+@ImportResource("classpath*:applicationContext.xmlc")
 @Slf4j
 public class SpringToCloudwatchApplication {
 
