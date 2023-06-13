db8c0042fb0b9c2e9b649f467b79593560d84ac8
Robert Barrimond <robert@barrimond.com>
2023-06-13 14:57:10 +0000

    fixt typo

--- src/main/java/com/capgemini/aiops/springtocloudwatch/SpringToCloudwatchApplication.java
+++ src/main/java/com/capgemini/aiops/springtocloudwatch/SpringToCloudwatchApplication.java
@@ -14,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @EnableScheduling
-@ImportResource("classpath*:applicationContext.xmlc")
+@ImportResource("classpath*:applicationContext.xml")
 @Slf4j
 public class SpringToCloudwatchApplication {
 
